f553fae29e0b23367c475a1ef225c8043919e9f0
dk123sw <763301256@qq.com>
2017-07-22 16:52:28 +0800

    改变状态栏颜色

--- app/src/main/java/afloat/com/fileceshi/function/functionActivity.java
+++ app/src/main/java/afloat/com/fileceshi/function/functionActivity.java
@@ -1,15 +1,30 @@
 package afloat.com.fileceshi.function;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import afloat.com.fileceshi.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class functionActivity extends Activity {
+
+    @BindView(R.id.button_use1)
+    Button mButtonUse1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_function);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_use1)
+    public void onViewClicked() {
+        Intent intent = new Intent(functionActivity.this , StatusActivity.class);
+        startActivity(intent);
     }
 }

bcc56f007b88198e2dac0e7e706c2c8527106012
dk123sw <763301256@qq.com>
2017-07-28 18:18:42 +0800

    工具类测试

--- app/src/main/java/afloat/com/fileceshi/KApplication.java
+++ app/src/main/java/afloat/com/fileceshi/KApplication.java
@@ -1,18 +1,3 @@
-/**
- * Copyright (C) 2015. Keegan小钢（http://keeganlee.me）
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package afloat.com.fileceshi;
 
 import android.app.ActivityManager;
@@ -20,6 +5,7 @@ import android.app.Application;
 import android.content.Context;
 import android.util.Log;
 
+import com.blankj.utilcode.util.Utils;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMOptions;
 import com.hyphenate.easeui.controller.EaseUI;
@@ -68,6 +54,7 @@ public class KApplication extends Application {
         setInstance(this);
 //        appAction = new AppActionImpl(this);
         mContext = getApplicationContext();
+        Utils.init(mContext);
         UMShareAPI.get(this);
         initEasemob();
         //微信
--- app/src/main/java/afloat/com/fileceshi/function/functionActivity.java
+++ app/src/main/java/afloat/com/fileceshi/function/functionActivity.java
@@ -3,6 +3,7 @@ package afloat.com.fileceshi.function;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import afloat.com.fileceshi.R;
@@ -12,8 +13,11 @@ import butterknife.OnClick;
 
 public class functionActivity extends Activity {
 
-    @BindView(R.id.button_use1)
-    Button mButtonUse1;
+
+    @BindView(R.id.status_test)
+    Button mStatusTest;
+    @BindView(R.id.tool_test)
+    Button mToolTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +26,19 @@ public class functionActivity extends Activity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.button_use1)
-    public void onViewClicked() {
-        Intent intent = new Intent(functionActivity.this , StatusActivity.class);
-        startActivity(intent);
+
+    @OnClick({R.id.status_test, R.id.tool_test})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.status_test:
+                Intent intent = new Intent(this, StatusActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.tool_test:
+                Intent intent1 = new Intent();
+                intent1.setClass(this , ToolTestActivity.class);
+                startActivity(intent1);
+                break;
+        }
     }
 }
